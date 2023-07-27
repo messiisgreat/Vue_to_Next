c645d045f276461ba47b0cc508238124022229fa
Superbluestar <alexander116gm@gmail.com>
2023-07-27 11:18:08 +0900

    fix childrens

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -9,7 +9,7 @@ export const metadata = {
   },
 }
 
-export default function Layout({ childrens }) {
+export default function Layout({ children }) {
   return (
     <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
       <body className="flex min-h-full flex-col">
